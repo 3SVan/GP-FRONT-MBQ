eb9a1f830ae6b1f9153c5554a07d0b4c5f2f3a51
EliasibContre <cesareliasib018@gmail.com>
2026-01-30 15:59:35 -0600

    New debbug point added

--- src/api/client.js
+++ src/api/client.js
@@ -2,6 +2,8 @@
 import axios from "axios";
 
 const baseURL = import.meta.env.VITE_API_URL || "http://localhost:3001";
+console.log("API baseURL =", baseURL);
+
 
 const api = axios.create({
   baseURL,
