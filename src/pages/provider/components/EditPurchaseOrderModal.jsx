e1915e880efc983eb4c34617d866411f541a29f4
EliasibContre <cesareliasib018@gmail.com>
2026-02-18 16:57:08 -0600

    Calendar positions

--- src/pages/provider/components/EditPurchaseOrderModal.jsx
+++ src/pages/provider/components/EditPurchaseOrderModal.jsx
@@ -98,9 +98,8 @@ export default function EditPurchaseOrderModal({
                   onChange={(e) =>
                     setForm((p) => ({ ...p, date: e.target.value }))
                   }
-                  className="w-full border rounded-2xl px-4 py-2.5 pr-12 outline-none focus:ring-2 focus:ring-lightBlue"
+                  className="w-full border rounded-2xl px-4 py-2.5  outline-none focus:ring-2 focus:ring-lightBlue"
                 />
-                <Calendar className="w-5 h-5 text-midBlue absolute right-4 top-1/2 -translate-y-1/2 pointer-events-none" />
               </div>
             </div>
 
